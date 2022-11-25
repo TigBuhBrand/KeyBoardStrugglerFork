69d9e97bdf5fb30befe7a5761724429dcdc46f37
Win10 <Win10@UKIDENT>
2022-11-25 15:32:20 +0100

    code: neu FakeNoMoreThanThat

---
+++ FakeNotMoreThanThat/Implementation/ThanThat.cs
@@ -0,0 +1,21 @@
+﻿using FakeNotMoreThanThat.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FakeNotMoreThanThat.Implementation
+{
+    public class ThanThat : IThanThat
+    {
+        public ThanThat(bool thakeItOrLeaveIt)
+        {
+            this.ThakeThat = thakeItOrLeaveIt;
+        }
+        public bool ThakeThat { get; }
+
+        public int PickIt(int countOfPickups)
+        {
+            return this.ThakeThat ? int.MaxValue / 2  : int.MinValue;
+        }
+    }
+}
